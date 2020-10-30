32bcf16bc57ae121920d8cd0fbac2eba7756674f
Vadim Inshakov <vadiminshakov@gmail.com>
2020-10-30 16:06:28 +0500

    add example for reading last actual config block for some usual tx block
    Signed-off-by: Vadim Inshakov <vadiminshakov@gmail.com>

---
+++ examples/readlastconfig/go.mod
@@ -0,0 +1,5 @@
+module github.com/newity/crawler/examples/readlastconfig
+
+go 1.14
+
+require github.com/newity/crawler v0.0.0-20201030103103-d41160eb89ad // indirect
---
+++ examples/readlastconfig/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"github.com/newity/crawler"
+	"github.com/newity/crawler/blocklib"
+	"github.com/sirupsen/logrus"
+)
+
+const (
+	CHANNEL = "mychannel"
+	USER    = "User1"
+	ORG     = "Org1"
+)
+
+func main() {
+	engine, err := crawler.New("connection.yaml", crawler.WithAutoConnect(USER, ORG))
+	if err != nil {
+		logrus.Error(err)
+	}
+
+	err = engine.Listen(crawler.FromBlock(), crawler.WithBlockNum(0))
+	if err != nil {
+		logrus.Error(err)
+	}
+
+	listener := engine.ListenerForChannel(CHANNEL)
+
+	for blockevent := range listener {
+		block, err := blocklib.FromFabricBlock(blockevent.Block)
+		if err != nil {
+			logrus.Error(err)
+		}
+		index, err := block.LastConfig()
+		if err != nil {
+			logrus.Error(err)
+		}
+		logrus.Infof("[%s] Last config index for block %d is %d\n", CHANNEL, block.Number(), index)
+	}
+
+	engine.StopListenAll()
+}
