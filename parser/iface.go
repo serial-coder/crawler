e64fa678b7e1041f2c551a73a22935a3e55f064f
Vadim Inshakov <vadiminshakov@gmail.com>
2020-10-31 15:35:06 +0500

    change Parser interface
    Signed-off-by: Vadim Inshakov <vadiminshakov@gmail.com>

--- parser/iface.go
+++ parser/iface.go
@@ -5,7 +5,11 @@ SPDX-License-Identifier: Apache-2.0
 
 package parser
 
+import "github.com/hyperledger/fabric-protos-go/common"
+
 // Parser serves as a contract for the implementation of the logic responsible for processing data from the blockchain.
 // Simply put, this is about how exactly and into what constituent parts we will disassemble the blocks.
 type Parser interface {
+	// Parse is responsible for parsing passed block
+	Parse(block *common.Block) (*Data, error)
 }
