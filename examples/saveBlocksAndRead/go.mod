c4fb378458ba271b6567a2efeed0c1c5a41a3e63
Vadim Inshakov <vadiminshakov@gmail.com>
2020-10-31 18:22:17 +0500

    add new example
    Signed-off-by: Vadim Inshakov <vadiminshakov@gmail.com>

---
+++ examples/saveBlocksAndRead/go.mod
@@ -0,0 +1,5 @@
+module github.com/newity/crawler/examples/readlastconfig
+
+go 1.14
+
+require github.com/newity/crawler v0.0.0-20201031110954-75f1c46e6b92 // indirect
---
+++ examples/saveBlocksAndRead/main.go
@@ -0,0 +1,67 @@
+/*
+Copyright LLC Newity. All Rights Reserved.
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package main
+
+import (
+	"encoding/hex"
+	"fmt"
+	"github.com/newity/crawler"
+	"github.com/sirupsen/logrus"
+	"time"
+)
+
+const (
+	CHANNEL = "cc"
+	USER    = "User1"
+	ORG     = "atomyze"
+)
+
+func main() {
+	engine, err := crawler.New("connection.yaml", crawler.WithAutoConnect(USER, ORG))
+	if err != nil {
+		logrus.Error(err)
+	}
+
+	err = engine.Listen(crawler.FromBlock(), crawler.WithBlockNum(0))
+	if err != nil {
+		logrus.Error(err)
+	}
+
+	go engine.Run()
+
+	// wait for pulling blocks to storage
+	time.Sleep(1 * time.Second)
+
+	readBlock(engine, 2)
+	engine.StopListenAll()
+}
+
+func readBlock(engine *crawler.Crawler, num int) {
+	data, err := engine.GetBlock(num)
+	if err != nil {
+		logrus.Error(err)
+	}
+
+	logrus.Infof("block %d with hash %s and prebious hash %s\n\nOrderers signed:\n", data.BlockNumber,
+		hex.EncodeToString(data.Datahash),
+		hex.EncodeToString(data.Prevhash))
+	for _, signature := range data.BlockSignatures {
+		fmt.Printf("MSP ID: %s\nSignature: %s\nCertificate:\n%s\n", signature.MSPID, hex.EncodeToString(signature.Signature), string(signature.Cert))
+	}
+	fmt.Println("Transactions")
+	for _, tx := range data.Txs {
+		t, err := tx.Timestamp()
+		if err != nil {
+			logrus.Error(err)
+		}
+		txid, err := tx.TxId()
+		if err != nil {
+			logrus.Error(err)
+		}
+
+		fmt.Printf("Tx ID: %s\nCreation time: %s\n", txid, t.String())
+	}
+}
