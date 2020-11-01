d55bb4ba0f7878890ed87ab12e189ff67c7e6c84
Vadim Inshakov <vadiminshakov@gmail.com>
2020-11-01 15:56:24 +0500

    make storage more abstract
    Signed-off-by: Vadim Inshakov <vadiminshakov@gmail.com>

--- storage/badger.go
+++ storage/badger.go
@@ -36,19 +36,15 @@ func (b *Badger) InitChannelStorage(channel string) error {
 	return nil
 }
 
-// Put saves parser.Data using block number as key
-func (b *Badger) Put(data *parser.Data) error {
-	encoded, err := Encode(data)
-	if err != nil {
-		return err
-	}
+// Put saves value by key
+func (b *Badger) Put(key string, value []byte) error {
 	return b.db.Update(func(txn *badger.Txn) error {
 		return txn.Set([]byte(strconv.Itoa(int(data.BlockNumber))), encoded)
 	})
 }
 
-// Get retrieves parser.Data from BadgerDB using block number as key
-func (b *Badger) Get(key string) (*parser.Data, error) {
+// Get retrieves data from BadgerDB using key
+func (b *Badger) Get(key string) ([]byte, error) {
 	var value []byte
 	err := b.db.View(func(txn *badger.Txn) error {
 		item, err := txn.Get([]byte(key))
@@ -61,11 +57,8 @@ func (b *Badger) Get(key string) (*parser.Data, error) {
 		})
 		return err
 	})
-	decoded, err := Decode(value)
-	if err != nil {
-		return nil, err
-	}
-	return decoded, err
+
+	return value, err
 }
 
 func (b *Badger) Delete(key string) error {
@@ -76,23 +69,4 @@ func (b *Badger) Delete(key string) error {
 
 func (b *Badger) Close() error {
 	return b.db.Close()
-}
-
-func Encode(data *parser.Data) ([]byte, error) {
-	var bytebuffer bytes.Buffer
-	e := gob.NewEncoder(&bytebuffer)
-	if err := e.Encode(data); err != nil {
-		return nil, err
-	}
-	return bytebuffer.Bytes(), nil
-}
-
-func Decode(data []byte) (*parser.Data, error) {
-	decoded := &parser.Data{}
-	bytebuffer := bytes.NewBuffer(data)
-	d := gob.NewDecoder(bytebuffer)
-	if err := d.Decode(&decoded); err != nil {
-		return nil, err
-	}
-	return decoded, nil
 }
--- storage/storage.go
+++ storage/storage.go
@@ -5,16 +5,14 @@ SPDX-License-Identifier: Apache-2.0
 
 package storage
 
-import "github.com/newity/crawler/parser"
-
 // Storage interface is a contract for storage implementations
 type Storage interface {
 	// init storage (initial setup of storage and connection create operations)
 	InitChannelStorage(channel string) error
-	// put parser.Data to storage
-	Put(data *parser.Data) error
-	// get parser.Data from storage by specified key
-	Get(key string) (*parser.Data, error)
+	// put value by key
+	Put(key string, value []byte) error
+	// get data from storage by specified key
+	Get(key string) ([]byte, error)
 	// remove parser.Data from storage
 	Delete(key string) error
 	// close connection to storage (network connection or file descriptor)
