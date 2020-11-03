003388831276209e3afb636cb28061d2e4589286
Vadim Inshakov <vadiminshakov@gmail.com>
2020-11-03 22:10:56 +0500

    add pubsub usage example
    Signed-off-by: Vadim Inshakov <vadiminshakov@gmail.com>

---
+++ examples/pubsub/main.go
@@ -0,0 +1,85 @@
+/*
+Copyright LLC Newity. All Rights Reserved.
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package main
+
+import (
+	"encoding/hex"
+	"fmt"
+	"github.com/newity/crawler"
+	"github.com/newity/crawler/storage"
+	"github.com/newity/crawler/storageadapter"
+	"github.com/sirupsen/logrus"
+	"google.golang.org/api/option"
+	"time"
+)
+
+const (
+	CHANNEL = "fiat"
+	USER    = "User1"
+	ORG     = "atomyze"
+)
+
+func main() {
+	pubsub, err := storage.NewPubSub("hlf-newity", option.WithCredentialsFile("/home/vadim/tests/pubsub/hlf-newity-9501093ed64e.json"))
+	if err != nil {
+		logrus.Fatal(err)
+	}
+
+	err = pubsub.InitChannelsStorage([]string{CHANNEL})
+	if err != nil {
+		logrus.Fatal(err)
+	}
+
+	engine, err := crawler.New("connection.yaml", crawler.WithStorage(pubsub), crawler.WithStorageAdapter(storageadapter.NewPubSubAdapter(pubsub)))
+	if err != nil {
+		logrus.Fatal(err)
+	}
+
+	err = engine.Connect(CHANNEL, USER, ORG)
+	if err != nil {
+		logrus.Fatal(err)
+	}
+
+	err = engine.Listen(crawler.FromBlock(), crawler.WithBlockNum(0))
+	if err != nil {
+		logrus.Fatal(err)
+	}
+
+	go engine.Run()
+
+	// wait for pulling blocks to storage
+	time.Sleep(6 * time.Second)
+
+	readFromQueue(engine, CHANNEL)
+	engine.StopListenAll()
+}
+
+func readFromQueue(engine *crawler.Crawler, topic string) {
+	data, err := engine.GetFromStorage(topic)
+	if err != nil {
+		logrus.Error(err)
+	}
+
+	logrus.Infof("block %d with hash %s and prebious hash %s\n\nOrderers signed:\n", data.BlockNumber,
+		hex.EncodeToString(data.Datahash),
+		hex.EncodeToString(data.Prevhash))
+	for _, signature := range data.BlockSignatures {
+		fmt.Printf("MSP ID: %s\nSignature: %s\nCertificate:\n%s\n", signature.MSPID, hex.EncodeToString(signature.Signature), string(signature.Cert))
+	}
+	fmt.Println("Transactions")
+	for _, tx := range data.Txs {
+		t, err := tx.Timestamp()
+		if err != nil {
+			logrus.Error(err)
+		}
+		txid, err := tx.TxId()
+		if err != nil {
+			logrus.Error(err)
+		}
+
+		fmt.Printf("Tx ID: %s\nCreation time: %s\n", txid, t.String())
+	}
+}
