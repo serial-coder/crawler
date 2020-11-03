5ead5fdde00e5681d7269f818fe6e9bf709ebc76
Vadim Inshakov <vadiminshakov@gmail.com>
2020-11-03 17:54:31 +0500

    add gob-encoding helpers
    Signed-off-by: Vadim Inshakov <vadiminshakov@gmail.com>

---
+++ storageadapter/gobs.go
@@ -0,0 +1,26 @@
+package storageadapter
+
+import (
+	"bytes"
+	"encoding/gob"
+	"github.com/newity/crawler/parser"
+)
+
+func Encode(data *parser.Data) ([]byte, error) {
+	var bytebuffer bytes.Buffer
+	e := gob.NewEncoder(&bytebuffer)
+	if err := e.Encode(data); err != nil {
+		return nil, err
+	}
+	return bytebuffer.Bytes(), nil
+}
+
+func Decode(data []byte) (*parser.Data, error) {
+	decoded := &parser.Data{}
+	bytebuffer := bytes.NewBuffer(data)
+	d := gob.NewDecoder(bytebuffer)
+	if err := d.Decode(&decoded); err != nil {
+		return nil, err
+	}
+	return decoded, nil
+}
