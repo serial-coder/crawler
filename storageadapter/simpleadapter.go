7b38295e8ded53e0f87916ca45f080e36ab2feeb
Vadim Inshakov <vadiminshakov@gmail.com>
2020-11-03 17:54:50 +0500

    del unused
    Signed-off-by: Vadim Inshakov <vadiminshakov@gmail.com>

--- storageadapter/simpleadapter.go
+++ storageadapter/simpleadapter.go
@@ -1,8 +1,6 @@
 package storageadapter
 
 import (
-	"bytes"
-	"encoding/gob"
 	"github.com/newity/crawler/parser"
 	"github.com/newity/crawler/storage"
 	"strconv"
@@ -30,23 +28,4 @@ func (s *SimpleAdapter) Retrieve(blocknum int) (*parser.Data, error) {
 		return nil, err
 	}
 	return Decode(value)
-}
-
-func Encode(data *parser.Data) ([]byte, error) {
-	var bytebuffer bytes.Buffer
-	e := gob.NewEncoder(&bytebuffer)
-	if err := e.Encode(data); err != nil {
-		return nil, err
-	}
-	return bytebuffer.Bytes(), nil
-}
-
-func Decode(data []byte) (*parser.Data, error) {
-	decoded := &parser.Data{}
-	bytebuffer := bytes.NewBuffer(data)
-	d := gob.NewDecoder(bytebuffer)
-	if err := d.Decode(&decoded); err != nil {
-		return nil, err
-	}
-	return decoded, nil
 }
